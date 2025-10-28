23c13264823398e6d800af00fb1d5debd787f811
Carlos Eduardo <eduardosantoos573@gmail.com>
2025-10-27 21:39:54 -0300

    feat: insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,5 +25,10 @@ public class Program {
     System.out.println("\n=== TEST 3: seller findAll ===");
     list = sellerDao.findAll();
     list.forEach(System.out::println);
+
+    System.out.println("\n=== TEST 4: seller insert ===");
+    Seller newSeller = new Seller(null, "Jackson", "jackson@gmail.com", LocalDate.now(), 4000.00, department);
+    sellerDao.insert(newSeller);
+    System.out.println("Inserted! New id = " + newSeller.getId());
   }
 }
